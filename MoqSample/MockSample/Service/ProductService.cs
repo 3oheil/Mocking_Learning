b802ddad70caef715ef538368c5fe3542b60b6d7
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-09-05 07:41:19 -0800

    add implement

--- MoqSample/MockSample/Service/ProductService.cs
+++ MoqSample/MockSample/Service/ProductService.cs
@@ -1,6 +1,15 @@
-﻿namespace MockSample.Service
+﻿using MockSample.Interface;
+
+namespace MockSample.Service
 {
     public class ProductService
     {
+        private readonly IProductRepository _repository;
+        public ProductService(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public int GetProductCount() => _repository.GetAll().Count();
     }
 }
